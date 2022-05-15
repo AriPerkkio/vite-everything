1af0278795a0efa1875ce33be12a5dea039c7d9a
Ari Perkkiö <ari.perkkio@gmail.com>
2022-05-15 11:07:35 +0000

    chore(dev-deps): add eslint + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+/** @type {import('eslint/lib/shared/types').ConfigData} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+};
