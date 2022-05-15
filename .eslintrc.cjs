170220f8984f6a84824e58e79a11ef0f81aff226
Ari Perkkiö <ari.perkkio@gmail.com>
2022-05-15 11:11:28 +0000

    chore: enable eslint-plugin-react/jsx-runtime rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
     ],
