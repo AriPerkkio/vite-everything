2e5a31a37795e6cd97764c30942787809c6cf9d8
Ari Perkkiö <ari.perkkio@gmail.com>
2022-05-30 16:19:12 +0000

    build: fix storybook hmr port

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,10 +1,16 @@
+import { mergeConfig, UserConfig } from 'vite';
 import type { StorybookViteConfig } from '@storybook/builder-vite';
 
 const config: StorybookViteConfig = {
     stories: ['../../src/**/*.stories.tsx'],
     core: { builder: '@storybook/builder-vite' },
     async viteFinal(config) {
-        return config;
+        const viteConfig: UserConfig = {
+            // Fixes HMR in Github Codespaces
+            server: { hmr: { clientPort: 443 } },
+        };
+
+        return mergeConfig(config, viteConfig);
     },
 };
 
