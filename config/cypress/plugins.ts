0e282c86eefa3766f25b374f5562518e6ecce3db
Ari Perkkiö <ari.perkkio@gmail.com>
2022-07-06 08:40:12 +0000

    test: cypress 9 after run cwd

--- config/cypress/plugins.ts
+++ config/cypress/plugins.ts
@@ -1,6 +1,11 @@
 import { startDevServer } from '@cypress/vite-dev-server';
 
 export default function (on, config: Cypress.Config) {
+    on('after:run', () => {
+        console.log('[after:run] cwd is', process.cwd());
+        // [after:run] cwd is /workspaces/vite-everything
+    });
+
     on('dev-server:start', (options) => {
         return startDevServer({ options });
     });
